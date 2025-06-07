a5e160a60735bec83ed95cf6427aad0d940587fb
Pedro Henrique <dev.pedrohenrique.contato@gmail.com>
2025-06-07 20:31:32 -0300

    Remove cache do gh-pages e node_modules do versionamento

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
                 life={1}
             />
 
-            <Router>
+            <Router basename="/Portifolio">
                 <Navbar />
                 <Routes>
                     <Route path='/' element={<Home />} />
